a1a9069930e6f8e538aae7f556ce9e6463e2d27f
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-23 18:56:21 -0400

    code cleanup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject image-ga "0.1"
+  :description "Image evolving GA"
+   :url "https://github.com/yogthos/ga-image-evolver-test"
+   :license {:name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html"}
+   :dependencies [[org.clojure/clojure "1.4.0"]
+                  [org.jaitools/jt-all "1.2.0"]])
